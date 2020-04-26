fc1f20ff368b5d85cb4d1b68ed3e87597fcbf170
assadiandre <assadiandre@gmail.com>
2020-04-25 20:24:16 -0700

    IT WORKS

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import populartimes
 import sys 
 
-# data = populartimes.get_id( "AIzaSyBb10HJkDvVvJQThvtmulz7gkrzQJCrZAA", "ChIJCYYAFMV9hYARmJORQa4TX58" ) 
+data = populartimes.get_id( "AIzaSyBb10HJkDvVvJQThvtmulz7gkrzQJCrZAA", "ChIJCYYAFMV9hYARmJORQa4TX58" ) 
 
 
-#return {"id": "ChIJCYYAFMV9hYARmJORQa4TX58", "name": "Safeway"}
-print("hello:[hello]")
+print(data)
 sys.stdout.flush()
 
 #AIzaSyBb10HJkDvVvJQThvtmulz7gkrzQJCrZAA
