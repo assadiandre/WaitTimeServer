469a5e6edc1b28f08f9d5a29a93ae6a81b085248
assadiandre <assadiandre@gmail.com>
2020-04-25 19:35:53 -0700

    changed main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import populartimes
 import sys 
 
-data = populartimes.get_id( "AIzaSyBb10HJkDvVvJQThvtmulz7gkrzQJCrZAA", "ChIJCYYAFMV9hYARmJORQa4TX58" ) 
+# data = populartimes.get_id( "AIzaSyBb10HJkDvVvJQThvtmulz7gkrzQJCrZAA", "ChIJCYYAFMV9hYARmJORQa4TX58" ) 
 
 
 #return {"id": "ChIJCYYAFMV9hYARmJORQa4TX58", "name": "Safeway"}
-print(data)
+print("hello:[hello]")
 sys.stdout.flush()
 
 #AIzaSyBb10HJkDvVvJQThvtmulz7gkrzQJCrZAA
